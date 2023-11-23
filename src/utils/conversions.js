46b8e3d8c814c1e852b355b2fb11cd3e2e8199dd
ashna khetan <ashnak@stanford.edu>
2023-11-23 00:45:11 -0500

    Updates

--- src/utils/conversions.js
+++
@@ -1,8 +0,0 @@
-const secondsToTimestamp = (sec) => {
-    const minutes = Math.floor(sec / 60);
-    const seconds = (sec % 60).toFixed(0);
-    const millis = Math.floor((sec % 1) * 1000);
-    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds + '.' + Math.floor(millis / 10);
-};
-
-export default secondsToTimestamp;
